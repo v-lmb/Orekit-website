66ff6fe68f8479cdc5733e64a071b0131bcc583e
Allix <thewitchonxanax@gmail.com>
2026-06-26 17:29:51 +0000

    feat: add shared data files for versions and governance members

---
+++ apps/web/app/data/governance.ts
@@ -0,0 +1,25 @@
+export const pmc = [
+  { name: 'Anne-Olivia Biramben',   org: 'CS Group',                       country: 'France',      linkedin: '' },
+  { name: 'Massimo Casasco',        org: 'ESA – ESTEC',                    country: 'Netherlands', linkedin: '' },
+  { name: 'Bryan Cazabonne',        org: 'Airbus Defence and Space',        country: 'France',      linkedin: '' },
+  { name: 'Paul Cefola',            org: 'University at Buffalo',           country: 'USA',         linkedin: '' },
+  { name: 'Sébastien Dinot',        org: 'CS Group',                       country: 'France',      linkedin: '' },
+  { name: 'Frank Dreger',           org: 'ESA – ESOC',                     country: 'Germany',     linkedin: '' },
+  { name: 'Hank Grabowski',         org: 'Independent expert',             country: 'USA',         linkedin: '' },
+  { name: 'Sébastien Herbinière',   org: 'Safran Data Systems',            country: 'France',      linkedin: '' },
+  { name: 'Petrus Hyvönen',         org: 'SSC',                            country: 'Sweden',      linkedin: '' },
+  { name: 'Yannick Jeandroz',       org: 'Airbus Defence and Space',        country: 'France',      linkedin: '' },
+  { name: 'Maxime Journot',         org: 'CS Group',                       country: 'France',      linkedin: '' },
+  { name: 'Christophe Le Bris',     org: 'Thales Alenia Space',            country: 'France',      linkedin: '' },
+  { name: 'Stéphanie Lizy-Destrez', org: 'ISAE-SUPAERO',                  country: 'France',      linkedin: '' },
+  { name: 'Luc Maisonobe',          org: 'Thales Alenia Space',            country: 'France',      linkedin: '' },
+  { name: 'Mark Rutten',            org: 'InTrack Solutions',              country: 'Netherlands', linkedin: '' },
+  { name: 'Romain Serra',           org: 'Independent expert',             country: 'France',      linkedin: '' },
+  { name: 'Evan Ward',              org: 'U.S. Naval Research Laboratory', country: 'USA',         linkedin: '' },
+]
+
+export const committers = [
+  { name: 'Bianca Buton',     org: 'CS Group',    country: 'France', linkedin: '' },
+  { name: 'Masayoshi Cerezo', org: 'Independent', country: 'Japan',  linkedin: '' },
+  { name: 'Pascal Parraud',   org: 'CS Group',    country: 'France', linkedin: '' },
+]
